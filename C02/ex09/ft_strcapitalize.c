a6e71777e60c38ddc6ccf250f5f8f57ca688b3bf
nolan2n <nolannvigina@gmail.com>
2024-04-27 19:43:45 +0200

    .\ft_strcapitalize.c

---
+++ C02/ex09/ft_strcapitalize.c
@@ -0,0 +1,62 @@
+#include <unistd.h>
+
+void    ft_putstr(char *str)
+{
+    int     i;
+
+    i = 0;
+    while (str[i])
+    {
+        write (1, &str[i], 1);
+        i++;
+    }
+}
+
+void    ft_lowercase(char *str)
+{
+    int     i;
+    
+    i = 0;
+    while (str[i])
+    {
+        if (str[i] >= 'A' && str[i] <= 'Z')
+            str[i] += 32;
+        i++;
+    }
+}
+
+int     check(char c)
+{
+    if (c >= '0' && c <= '9')
+        return (1);
+    if (c >= 'A' && c <= 'Z')
+        return (1);
+    if (c >= 'a' && c <= 'z')
+        return (1);
+    return (0);
+}
+
+char    *ft_strcapitalize(char *str)
+{
+    int     i;
+
+    i = 0;
+    ft_lowercase(str);
+    if (str[0] >= 'a' && str[0] <= 'z')
+        str[0] -= 32;
+    while (str[i])
+    {
+        if (i > 0 && !(check(str[i - 1])) && (str[i] >= 'a' && str[i] <= 'z'))
+            str[i] -= 32;
+        i++;
+    }
+    return (str);
+}
+
+int     main(void)
+{
+    char    test[] = "salut, comMent tu vas ? 42mots quarante-deux; cinquante+et+un";
+
+    ft_putstr(ft_strcapitalize(test));
+    return (0);
+}
