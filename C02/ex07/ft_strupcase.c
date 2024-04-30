000449539ad5986be3ee441c6286fe89a828a0e4
nolan2n <nolannvigina@gmail.com>
2024-04-30 23:57:06 +0200

    .\ft_strupcase.c

--- C02/ex07/ft_strupcase.c
+++ C02/ex07/ft_strupcase.c
@@ -2,14 +2,8 @@
 
 void    ft_putstr(char *str)
 {
-    int     i;
-
-    i = 0;
-    while (str[i])
-    {
-        write (1, &str[i], 1);
-        i++;
-    }
+    while (*str)
+        write (1, &*str++, 1);
 }
 
 char    *ft_strupcase(char *str)
