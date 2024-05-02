182d5beb1d02ad003e902794471cf2446836b891
nolan2n <nolannvigina@gmail.com>
2024-05-02 15:21:01 +0200

    .\ft_strncpy.c

--- C02/ex01/ft_strncpy.c
+++ C02/ex01/ft_strncpy.c
@@ -2,14 +2,8 @@
 
 void    ft_putstr(char *str)
 {
-    int     i;
-
-    i = 0;
-    while(str[i])
-    {
-        write (1, &str[i], 1);
-        i++;
-    }
+    while(*str)
+        write (1, &*str++, 1);
 }
 
 char    *ft_strncpy(char *dest, char *src, unsigned int n)
@@ -17,23 +11,17 @@ char    *ft_strncpy(char *dest, char *src, unsigned int n)
     unsigned int     i;
 
     i = 0;
-    while(src[i] && i < n)
-    {
-        dest[i] = src[i];
-        i++;
-    }
-    while (i < n)
-    {
-        dest[i] = '\0';
-        i++;
-    }
+    while(*src && --n)
+        *(dest + i++) = *src++;
+    while (n--)
+        *(dest + i++) = '\0';
     return (dest);
 }
 
 int     main(void)
 {
-    char    src[] = "hello world";
-    char    dest[40];
-    ft_putstr(ft_strncpy(dest, src, 8));
+    char    src[] = "hello";
+    char    dest[3];
+    ft_putstr(ft_strncpy(dest, src, 2));
     return (0);
 }
