ca1ea2f50d970553028e89966c2a75c5515884e3
nolan2n <nolannvigina@gmail.com>
2024-05-02 20:40:28 +0200

    .\ft_strncmp.c

--- C03/ex01/ft_strncmp.c
+++ C03/ex01/ft_strncmp.c
@@ -5,13 +5,13 @@ int ft_strncmp(char *s1, char *s2, unsigned int n)
     int     i;
 
     i = 0;
-    while((s1[i] || s2[i]) && --n)
+    while((*(s1 + i) && *(s2 + i)) && --n)
     {    
-        if(s1[i] != s2[i])
-            return(s1[i] - s2[i]);
+        if(*(s1 + i) != *(s2 + i))
+            return(*(s1 + i) - *(s2 + i));
         i++;
     }
-    return(s1[i] - s2[i]);
+    return(*(s1 + i) - *(s2 + i));
 }
 
 int     main(void)
@@ -19,7 +19,7 @@ int     main(void)
     char    s1[] = "hello";
     char    s2[] = "hella";
 
-    printf("s1[i] - s2[i] = %d\n", ft_strncmp(s1, s2, 4));
+    printf("*(s1 + i) - *(s2 + i) = %d\n", ft_strncmp(s1, s2, 4));
     return (0);
 }
 //for example s1 = "hello"  s2 = "hella" and n = 4 
