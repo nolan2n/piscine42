987e3ae15bccad143c0d41ccc9a934b09f9c41ac
nolan2n <nolannvigina@gmail.com>
2024-05-02 19:59:49 +0200

    .\ft_putnbr.c

---
+++ C04/ex02/ft_putnbr.c
@@ -0,0 +1,28 @@
+#include <unistd.h>
+void    ft_putchar(char c)
+{
+        write(1, &c, 1);
+}
+
+void    ft_putnbr(int nb)
+{
+    long l;
+
+    l = nb;
+    if (l < 0)
+    {
+        ft_putchar('-');
+        l *= -1;
+    }
+    if (l >= 10 )
+    {
+        ft_putnbr(l / 10);
+    }
+    ft_putchar((l  % 10) + '0');
+}
+
+int     main(void)
+{
+    ft_putnbr(-42315602);
+    return (0);
+}
