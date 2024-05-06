eb0154a7ba9f78636f87a5d4c68e8ee350f74d11
nolan2n <nolannvigina@gmail.com>
2024-05-06 19:38:36 +0200

    .\ft_ft.c

--- C01/ex00/ft_ft.c
+++ C01/ex00/ft_ft.c
@@ -1,4 +1,18 @@
+#include <stdio.h>
+
 void    ft_ft(int *nbr)
 {
     *nbr = 42;
+}
+
+int     main(void)
+{
+    int     a;
+    int     *p;
+    
+    a = 28;
+    printf ("before dereferencing : %d\n", a);
+    p = &a;
+    ft_ft(p);
+    printf ("after dereferencing : %d\n", a);
 }
