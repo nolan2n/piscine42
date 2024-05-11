7b7b6a718f4fb05df68a88b0f01a0a44fec02331
nolan2n <nolannvigina@gmail.com>
2024-05-11 22:22:48 +0200

    recursive

---
+++ C01/ex05/ft_putstr_recursive.c
@@ -0,0 +1,28 @@
+#include <unistd.h>
+#include <stdio.h>
+
+int     ft_strlen(char *str)
+{
+    int     i;
+
+    i = 0;
+    while (*(str + i))
+        i++;
+    return (i);
+}
+
+char    *ft_putstr_recursive(char *str, int Strlen)
+{
+    if (Strlen > 0)
+        ft_putstr_recursive(str, Strlen - 1); 
+    write(1, &*(str + Strlen), 1);
+}
+
+int     main(void)
+{
+    char    test[] = "Hello";
+    int     Strlen;
+    Strlen = ft_strlen(test) - 1;
+    ft_putstr_recursive(test, Strlen);
+    return (0);
+}
---
+++ C01/ex05/ft_reverse_putstr_recursive.c
@@ -0,0 +1,32 @@
+#include <unistd.h>
+#include <stdio.h>
+
+int     ft_strlen(char *str)
+{
+    int     i;
+
+    i = 0;
+    while (*(str + i))
+        i++;
+    return (i);
+}
+
+void    ft_reverse_putstr_recursive(char *str)
+{
+    int     Strlen;
+    int     i;
+
+    i = 0;
+    Strlen = ft_strlen(str) - 1;
+    if (i < Strlen)
+        ft_reverse_putstr_recursive(str + (i + 1));
+    write(1, &*(str + i), 1);
+}
+
+
+int     main(void)
+{
+    char    test[] = "Hello";
+    ft_reverse_putstr_recursive(test);
+    return (0);
+}
