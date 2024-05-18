12e77e6d7d3ecfb2d3b6d687af7c8d49ea6a093e
nolan2n <nolannvigina@gmail.com>
2024-05-18 19:03:22 +0200

    .\ft_rev_int_tab.c

--- C01/ex07/ft_rev_int_tab.c
+++ C01/ex07/ft_rev_int_tab.c
@@ -1,8 +1,21 @@
 #include <unistd.h>
+#include <stdio.h>
 
 void ft_putchar(char c)
 {
     write(1, &c, 1);
+}
+
+void    ft_putnbr(int nb)
+{
+    if (nb < 0)
+    {
+        ft_putchar('-');
+        nb *= -1;
+    }
+    if (nb >= 10 )
+         ft_putnbr(nb / 10);
+    ft_putchar((nb  % 10) + '0');
 }
 
 void    ft_rev_int_tab(int *tab, int size)
@@ -12,12 +25,12 @@ void    ft_rev_int_tab(int *tab, int size)
     i = 0;
     if (size > 1)
         ft_rev_int_tab(tab + (i + 1), size - 1);
-    ft_putchar(*(tab + i) + '0');
+    ft_putnbr(*(tab + i));
 }
 
 int main(void)
 {
-    int test[5] = {1, 2, 3, 4, 5};
+    int test[5] = {1, 20, 3, 4, 5};
 
     ft_rev_int_tab(test, 5);
     return (0);
